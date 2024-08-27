fce8187d6519b17037a8b49032959d9560ba6a7f
John Indakwa <108904759+JOHNINDAKWA@users.noreply.github.com>
2024-08-27 13:53:34 +0300

    Update Contact.jsx

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className="contact-section">
         <h3>Customer service</h3>
         <p>
-          <a href="mailto:orders@jamiebeck.co">orders@jamiebeck.co</a>
+          <a href="mailto:orders@johnindakwa@gmail.co">johnindakwa@gmail.com</a>
         </p>
       </div>
       <hr />
@@ -15,26 +15,26 @@ const Contact = () => {
         <h3>Speaking engagements, brand collaborations or social media collaborations</h3>
         <p>Management, Digital Brand Architects</p>
         <p>
-          Vanessa Flaherty — <a href="mailto:Vanessa@thedigitalbrandarchitects.com">Vanessa@thedigitalbrandarchitects.com</a>
+          Ballet Awkandy — <a href="mailto:Vanessa@thedigitalbrandarchitects.com">balletawkandy@gmail.com</a>
         </p>
       </div>
       <hr />
       <div className="contact-section">
         <h3>Literary Agents</h3>
-        <p>The Gernert Company</p>
+        <p>The Tawi Company</p>
         <p>
-          Seth Fishman — <a href="mailto:sfishman@thegernertco.com">sfishman@thegernertco.com</a>
+          Seth Olee — <a href="mailto:sfishman@thegernertco.com">solee@gmail.com</a>
         </p>
         <p>
-          Anna Worrall — <a href="mailto:aworrall@thegernertco.com">aworrall@thegernertco.com</a>
+          Anna Wabuko— <a href="mailto:aworrall@thegernertco.com">annawabuko@gmail.com</a>
         </p>
       </div>
       <hr />
       <div className="contact-section">
         <h3>Press and Media Contacts</h3>
-        <p>Simon & Schuster</p>
+        <p>Simon &  Mitchelle</p>
         <p>
-          Jessica Preeg — <a href="mailto:jessica.preeg@simonandschuster.com">jessica.preeg@simonandschuster.com</a>
+          John Kulundu — <a href="mailto:jessica.preeg@simonandschuster.com">johnkulundu@gmail.com</a>
         </p>
       </div>
     </div>
